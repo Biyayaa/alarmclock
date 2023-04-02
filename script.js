350012d0e3f757e52055ec4138edb7c0374b7835
Biyayaa <onaleye.ap@gmail.com>
2023-04-02 06:44:07 +0100

    made the alarm clock 12-hour format

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
 let time = document.getElementById("time");
-let date,hours,minutes,seconds;
+let date,hours,minutes,seconds,amPm;
 
 function updateTime() {
   date = new Date();
    seconds = date.getSeconds();
    hours = date.getHours();
    minutes = date.getMinutes();
+
+
+   amPm = hours >= 12 ? 'PM' : 'AM'; // check if it's AM or PM
+   hours = hours % 12 || 12; // convert to 12-hour format
 
   if (hours < 10) {
     hours = "0" + hours;
@@ -16,7 +20,7 @@ function updateTime() {
   if (seconds < 10) {
     seconds = "0" + seconds;
   }
-  let currentTime = hours + ":" + minutes + ":" + seconds;
+  let currentTime = hours + ":" + minutes + ":" + seconds + " " + amPm;
   time.innerHTML = currentTime;
 }
 setInterval(updateTime, 1000);
@@ -27,12 +31,13 @@ setInterval(updateTime, 1000);
 let hourss = document.getElementById('hourss');
 let minutess = document.getElementById('minutess');
 let secondss = document.getElementById('secondss');
+let amPms = document.getElementById('amPms')
 let alarmTimeout;
 let audio = document.getElementById("audio");
 
 function alarmSet() {
   let ring=document.getElementById('ring')
-  if (hours == hourss.value && minutes == minutess.value && seconds == secondss.value) {
+  if (hours == hourss.value && minutes == minutess.value && seconds == secondss.value && amPm == amPms.value) {
     ring.innerHTML = "alarm is ringing";
     audio.play();
     // clearTimeout(alarmTimeout);
